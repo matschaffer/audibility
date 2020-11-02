8155d512417998ff26445dc6af7f76c410452bba
Mat Schaffer <mat@elastic.co>
2020-11-02 03:12:09 +0000

    A super ugly basic UI

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,91 @@
 import * as Tone from "tone";
 import { hash, audiolize } from "./player";
+import * as Nexus from "nexusui";
 
-// import * as NexusUI from "nexusui";
-
-// var button = new Nexus.Button("#target");
-
-const playButton = document.querySelector("button");
-
-if (playButton) {
-  playButton.addEventListener("click", () => {
-    if (Tone.context.state !== "running") {
-      Tone.context.resume().then(() => {
-        Tone.Transport.start();
-
-        let synth = new Tone.Synth().toDestination();
-
-        audiolize("us-east-1", "service-docker-*", (event) => {
-          let scaleElement = document.querySelector<HTMLInputElement>("#scale");
-          if (scaleElement) {
-            let scale = scaleElement.value.split(" ");
-            let note = scale[hash(event.docker.container.name) % scale.length];
-            try {
-              synth.triggerAttackRelease(note, "16n");
-            } catch (e) {
-              console.log(`Bad note ${note}`);
-            }
-          }
-        });
-      });
-    }
-  });
+function ensureTransport(callback: () => void) {
+  if (Tone.context.state !== "running") {
+    Tone.context.resume().then(() => {
+      Tone.Transport.start();
+      callback();
+    });
+  }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const controls = document.getElementById("controls");
+  if (controls) {
+    let regionElement = controls.querySelector<HTMLInputElement>(
+      "input[name=region]"
+    );
+    let indexPatternElement = controls.querySelector<HTMLInputElement>(
+      "input[name=indexPattern]"
+    );
+
+    let run = new Nexus.Toggle(controls.querySelector(".run"));
+
+    let pan = new Nexus.Dial(controls.querySelector(".pan"), {
+      min: -1,
+      max: 1,
+      value: 0,
+    });
+
+    let offsetDial = new Nexus.Dial(controls.querySelector(".offset .dial"), {
+      min: -3,
+      max: 3,
+      value: 0,
+    });
+    let offsetNumber = new Nexus.Number(
+      controls.querySelector(".offset .number")
+    );
+    offsetNumber.link(offsetDial);
+
+    let notes = new Nexus.Sequencer(controls.querySelector(".notes"), {
+      rows: 3,
+      columns: 12,
+    });
+    notes.matrix.set.cell(0, 1, 1);
+    notes.matrix.set.cell(2, 1, 1);
+    notes.matrix.set.cell(4, 1, 1);
+
+    run.on("change", (state: boolean) => {
+      if (state) {
+        ensureTransport(() => {
+          let panner = new Tone.Panner(0).toDestination();
+          pan.on("change", (v: number) => {
+            panner.pan.setValueAtTime(v, "+0");
+          });
+          let synth = new Tone.Synth().connect(panner);
+
+          let region = regionElement ? regionElement.value : "";
+          let indexPattern = indexPatternElement
+            ? indexPatternElement.value
+            : "";
+
+          audiolize(region, indexPattern, (event) => {
+            let scale: number[] = [];
+            notes.matrix.pattern.forEach((row: boolean[], rowIndex: number) => {
+              row.forEach((state: boolean, columnIndex: number) => {
+                if (state) {
+                  scale.push(
+                    60 +
+                      (rowIndex - 1) * 12 +
+                      columnIndex +
+                      offsetDial.value * 12
+                  );
+                }
+              });
+            });
+
+            try {
+              let note = scale[hash(event.logger) % scale.length];
+              let frequency = Tone.Frequency(note, "midi").toFrequency();
+              synth.triggerAttackRelease(frequency, "16n");
+            } catch (e) {
+              console.log(`Couldn't play event: ${event}`);
+            }
+          });
+        });
+      }
+    });
+  }
+});
